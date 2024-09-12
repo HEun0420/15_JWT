df7578293aec67ac513a7e7aac15dca279d506b7
HEun0420 <usialeta@gmail.com>
2024-09-12 19:31:47 +0900

    jwt

--- src/main/java/com/ohgiraffers/jwt/test/controller/TestController.java
+++ src/main/java/com/ohgiraffers/jwt/test/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.ohgiraffers.jwt.test.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/test")
 public class TestController {
 
+    // user일 때 사용가능함
+    @PreAuthorize("hasAuthority('USER')")
     @GetMapping("/user")
     public String testUser() {
         return "user 권한만 접근 가능한 test success";
     }
 
+    // admin일 때 사용가능함
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/admin")
     public String testAdmin() {
         return "Admin 권한만 접근 가능한 test success";
